bbfe335526e57c56043e7d77113512f69e2a18ae
Kerem Can CELEPKOLU <88246848+keremclp@users.noreply.github.com>
2023-07-10 23:24:17 +0300

    Add files via upload

---
+++ recipeapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = '754509f3';
+
+  const APP_KEY = 'fbed5c7d17867614daca25af7c794144';
+
+  return (
+    <div className="App">
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
